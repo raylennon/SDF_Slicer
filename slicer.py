e531b6a1c0311b2e70f2e3a4c7b3ddeb34baf002
Ray Lennon <48305386+raylennon@users.noreply.github.com>
2025-11-13 04:43:15 +0000

    Slices as of 11/12

--- slicer.py
+++ slicer.py
@@ -2,17 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 
-a = np.array([1, 2, 3])
-
 # Printer resolution; this is machine-specific, not slice-specific!
 xres = 299.73  # DPI
 yres = 299.73  # DPI
-n_layers = 10
+n_layers = 111
 layer_height = 0.018
+
+# droplet size in mm, I think
+d_size = (1/xres) * 25.4
 
 # File prefix for outputted slices.
 prefix = "slice_"
-
 
 # Bounding area size in [mm]
 bounds = np.array([30, 30])
@@ -23,33 +23,58 @@ mapy = lambda y: yres * 0.0393701 * y
 x_samp = np.linspace(0, bounds[0], int(np.ceil(mapx(bounds[0]))))
 y_samp = np.linspace(0, bounds[1], int(np.ceil(mapy(bounds[1]))))
 x, y = np.meshgrid(x_samp, y_samp)
-print(f"Max value of x_samp: {np.max(x_samp)}")
+print(f"1 Droplet = {1e3*bounds[0]/len(x_samp):0.4} um")
 
 
 def circle(x, y, loc, r):
     return np.sqrt((x - loc[0]) ** 2 + (y - loc[1]) ** 2) - r
+def box(x,y,loc,size):
+    return np.maximum(x-loc[0], y-loc[1])-size/2
 
+base = 3
 
 # x,y in units of mm
 def domain(x, y, z):
-    radius = 12.7
+    if z/layer_height < base:
+        radius = 10
+    else:
+        radius = d_size*1
 
-    nx = 
+    # x_space = d_size * 10 # mm
+    # y_space = d_size * 10 # mm
+    x_space = 1 # mm
+    y_space = 1.5 # mm
 
-    return circle(x, y, [15, 15], radius)
+    ix_x = np.floor(x / x_space)
+    ix_y = np.floor(y / y_space)
+
+    x -= x_space * ix_x
+    y -= y_space * ix_y
+
+    cutoff = (ix_x < np.floor(bounds[0]/x_space)) * (ix_y < np.floor(bounds[1]/y_space))
+    return circle(x, y, 2*[radius], radius)
+    # return box(x, y, 2*[radius], 2*radius) * cutoff
 
 for i in range(n_layers):
     z = i * layer_height
 
-    # Here, only a single color is used. 
+    # Here, only a single color is used.
     data = 255 * (domain(x, y, z) < 0).astype(np.uint8)
-    
-    # Tiling into RGBA format. This means - for now - the file has white, opaque features on a background of (0,0,0,0) (transparent)
-    data = np.repeat(data[:,:,None],4,axis=2)
 
-    print (f"Data shape -> {data.shape}")
+    # Tiling into RGBA format. This means - for now - the file has white, opaque features on a background of (0,0,0,0) (transparent)
+    # data = np.repeat(data[:,:,None],4,axis=2)
+
+    # Prettier colors for vis
+    data = np.repeat(data[:,:,None],1,axis=2)
+    if i < base:
+        data = np.dstack((data, ~data, ~data, 255*np.ones(data.shape[:-1], dtype=np.uint8)))
+    else:
+        data = np.dstack((np.uint8(0.5*data), ~data, ~data, 255*np.ones(data.shape[:-1], dtype=np.uint8)))
+
+    # print (f"Data shape -> {data.shape}")
+    # print (f"Data first element -> {data[0,0,:]}")
     im = Image.fromarray(data)
-    im.save(f"outputs/slice_{i+1:02d}.png")
+    im.save(f"outputs/slice_{i+1:03d}.png")
 
 
 fig, ax = plt.subplots()
